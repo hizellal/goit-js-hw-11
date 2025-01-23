fdfa931155786bb15de87b1557436ce2bfd37135
hizellal <vladimir.liubshenko@gmail.com>
2025-01-23 04:56:11 +0200

    comm_before_finally

--- src/gallery.js
+++ src/gallery.js
@@ -1,4 +1,50 @@
-fetch('https://api.unsplash.com/search/photos?query=cat&client_id=JxtDohqrvLiUtsCQPJqUcZZFVHIZOMdWOqW05zEKHjE');
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const searchForm = document.querySelector('.js-search-form');
+const gallery = document.querySelector('.js-gallery');
+
+const loadGallery = imgInfo => {
+    return `
+    <li class='gallery-images'>
+        <img class='gallery-image' src='${imgInfo.urls.regular}' alt='${imgInfo.alt_description}' />
+    </li>
+    `;
+}
+
+const onBtnSubmit = event => {
+    event.preventDefault();
+
+    const searchedImage = event.currentTarget.elements.user_query.value.trim();
+    console.log(searchedImage);
+    console.log(event.currentTarget.elements);
+    
+    fetch(`https://api.unsplash.com/search/photos?query=${searchedImage}&client_id=JxtDohqrvLiUtsCQPJqUcZZFVHIZOMdWOqW05zEKHjE`)
+    .then(response => {
+        if (!response.ok) {
+          iziToast.error({
+          message: "Sorry, there are no images matching your search query. Please try again!",
+          messageColor: '#fff',
+          position: 'topRight',
+          color: '#ef4040',
+        });
+        }
+
+        return response.json();
+    })
+    .then(data => {
+        const galleryLoad = data.results.map(el => loadGallery(el)).join('');
+
+        gallery.innerHTML = galleryLoad;
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
+
+searchForm.addEventListener('submit', onBtnSubmit);
+
+//fetch('https://api.unsplash.com/search/photos?query=cat&client_id=JxtDohqrvLiUtsCQPJqUcZZFVHIZOMdWOqW05zEKHjE');
 
 
 
