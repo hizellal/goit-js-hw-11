e37a7174350bb82bfab0a9bcfc6e9c7875b7e141
hizellal <vladimir.liubshenko@gmail.com>
2025-01-24 04:24:05 +0200

    finnaly.comm

--- src/gallery.js
+++ src/gallery.js
@@ -1,13 +1,38 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const searchForm = document.querySelector('.js-search-form');
 const gallery = document.querySelector('.js-gallery');
+const loaderContainer = document.querySelector('.loader-container');
+
+const showLoader = () => {
+    loaderContainer.classList.remove('hidden');
+};
+
+const hideLoader = () => {
+    loaderContainer.classList.add('hidden');
+};
 
 const loadGallery = imgInfo => {
+    console.log(imgInfo);
     return `
     <li class='gallery-images'>
-        <img class='gallery-image' src='${imgInfo.urls.regular}' alt='${imgInfo.alt_description}' />
+    <a href='${imgInfo.largeImageURL}'>
+        <img 
+        class='gallery-image' 
+        src='${imgInfo.webformatURL}' 
+        alt='${imgInfo.tags.split(",").slice(0, 3)}' 
+        />
+    </a>
+        <div class='gallery-image-card js-gallery-image-card'>
+            <p class='image-card-likes'><span class="card-title">Likes</span><br>${imgInfo.likes}</p>
+            <p class='image-card-views'><span class="card-title">Views</span><br>${imgInfo.views}</p>
+            <p class='image-card-comments'><span class="card-title">Comments</span><br>${imgInfo.comments}</p>
+            <p class='image-card-downloads'><span class="card-title">Downloads</span><br>${imgInfo.downloads}</p>
+        </div>
     </li>
     `;
 }
@@ -16,10 +41,10 @@ const onBtnSubmit = event => {
     event.preventDefault();
 
     const searchedImage = event.currentTarget.elements.user_query.value.trim();
-    console.log(searchedImage);
-    console.log(event.currentTarget.elements);
-    
-    fetch(`https://api.unsplash.com/search/photos?query=${searchedImage}&client_id=JxtDohqrvLiUtsCQPJqUcZZFVHIZOMdWOqW05zEKHjE`)
+    //console.log(searchedImage);
+    showLoader();
+
+    fetch(`https://pixabay.com/api/?key=48415738-453a35c27b5ce388251d5c099&q=${searchedImage}&image_type=photo`)
     .then(response => {
         if (!response.ok) {
           iziToast.error({
@@ -33,40 +58,27 @@ const onBtnSubmit = event => {
         return response.json();
     })
     .then(data => {
-        const galleryLoad = data.results.map(el => loadGallery(el)).join('');
+        if (data.hits.length === 0) {
+            iziToast.error({
+                title: 'Error',
+                message: 'Sorry, there are no images matching your search query. Please try again!',
+                position: 'topRight',
+                color: '#ef4040',
+            });
+            return;
+        }
+        const galleryLoad = data.hits.map(el => loadGallery(el)).join('');
 
         gallery.innerHTML = galleryLoad;
+
+        new SimpleLightbox('.js-gallery a', { captionsData: 'alt', captionDelay: 250 });
     })
     .catch(err => {
         console.log(err);
+    })
+    .finally(() => {
+        hideLoader();
     });
 };
 
 searchForm.addEventListener('submit', onBtnSubmit);
-
-//fetch('https://api.unsplash.com/search/photos?query=cat&client_id=JxtDohqrvLiUtsCQPJqUcZZFVHIZOMdWOqW05zEKHjE');
-
-
-
-
-
-// fetch('https://jsonplaceholder.typicode.com/posts?_limit=10&_sort=body')
-//   .then(response => {
-//     console.log(response);
-
-//     if (!response.ok) {
-//         throw new Error(response.status);
-//     }
-
-//       return response.json();
-//    })
-//    .then(data => {
-//      console.log(data);
-//    })
-//    .catch(err => {
-//     console.dir(err);
-
-//     if(err.message === '404') {
-//         alert('Постів не знайдено!');
-//     }   
-//    });
